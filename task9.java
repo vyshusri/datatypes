ee57edc53538ac4ea9a67c9a011ff7ab65d26fbc
vyshusri <vyshusriram15@gmail.com>
2026-04-09 07:39:11 -0700

    com1

--- task9.java
+++ task9.java
@@ -1,8 +1,13 @@
 public class task9 {
-   static boolean isv=true;
+   static boolean vysh=true;
+   static boolean guddu=true;
     public static void main(String[] args) {
-        if(isv){
-            System.out.println("true");
-        }
+        if(vysh)
+            System.out.println(" vyshu is a dancer");
+            System.out.println("vyshu is not a dancer" );
+        if(guddu){
+            System.out.println("laddu is a singer");
+            System.out.println("laddu is a singer");
     }
+}
 }
