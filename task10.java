46541f39b723eb2cd18909c98797fd227ca8cf65
vyshusri <vyshusriram15@gmail.com>
2026-04-09 07:44:48 -0700

    com1

---
+++ task10.java
@@ -0,0 +1,13 @@
+public class task10 {
+    static int age=18;
+    public static void main(String[] args) {
+        if(age>=17){
+            System.out.println("You are eligible to vote");
+         } else{
+            System.out.println("You are not to vote");
+
+            }
+        }
+    }
+    
+
