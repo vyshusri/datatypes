4474c91c7cd172b589454cfb13c5c4f306aa08a7
vyshusri <vyshusriram15@gmail.com>
2026-04-09 09:35:58 -0700

    com

--- task10.java
+++ task10.java
@@ -3,10 +3,10 @@ public class task10 {
     public static void main(String[] args) {
         if(age>=18){
             System.out.println("You are eligible to vote");
-         } else{
+    }else{
             System.out.println("You are not eligible to vote");
-
-            }
+    }
+            
         }
     }
     
--- task11.java
+++ task11.java
@@ -1,12 +1,12 @@
 public class task11 {
     static int a=20;
     public static void main(String[] args[]){
-    if(a%2==0){
-        System.out.println(a+"is Even");
-    }else{
-        System.out.println(a+"is Odd");
+    if(a%2==0)
+        System.out.println("is Even");
+    else
+        System.out.println("is Odd");
 
-    }
     
-}
+    
+    }
 }
