cb8f5247862701c893522d24c770dd81c4be06b6
vyshusri <vyshusriram15@gmail.com>
2026-04-23 04:49:36 -0700

    1

---
+++ switch2.java
@@ -0,0 +1,29 @@
+public class switch2 {
+    static char grade='A';
+    public static void main(String[] args) {
+        switch(grade){
+        case 'A' :
+        System.out.println("Excellent");
+        break;
+        case 'B' :
+        System.out.println("Good");
+        break;
+        case 'C' :
+        System.out.println("Average");
+        break;
+        case 'D' :
+        System.out.println("Below Average");
+        break;
+        case 'F' :
+        System.out.println("Fail");
+        break;
+        default:
+        System.out.println("invalid grade");
+        break;
+    }
+    
+}
+    }
+    
+    
+    
