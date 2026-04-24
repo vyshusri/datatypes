b32e44993830e95adef42d71ea58704ee63cd0ea
vyshusri <vyshusriram15@gmail.com>
2026-04-24 01:46:09 -0700

    dowhile

---
+++ doWhile.java
@@ -0,0 +1,18 @@
+public class doWhile {
+    public static void main(String[] args) {
+        int i=1;
+        do{
+            System.out.println(i);
+            i++;
+        }
+        while(i<=10);
+    }
+    int j=10;
+    do{
+        System.out.println(j);
+        j--;
+    }
+    while(j>=1);
+}
+    
+
