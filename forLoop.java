bf1f22b96d285e611bf47506fa35f461f53d33fd
vyshusri <vyshusriram15@gmail.com>
2026-04-24 01:56:54 -0700

    forloop

---
+++ forLoop.java
@@ -0,0 +1,10 @@
+public class forLoop {
+    public static void main(String[] args) {
+        for(int i=1;i<11;i++){
+        System.out.println(i);
+    }
+    for(int j=10;j>1;j--){
+        System.out.println(j);
+    }
+}
+}
