541abd1039745d6e514bd764d551e760394ea7d8
vyshusri <vyshusriram15@gmail.com>
2026-04-24 02:15:43 -0700

    comm1

---
+++ allLoop.java
@@ -0,0 +1,23 @@
+public class allLoop {
+    static int i=0;
+    static int j=0;
+    static int k=0;
+    public static void main(String[] args) {
+        
+        while(i>0){
+            System.out.println(i);
+            i++;
+        }
+        do{
+        System.out.println(j);
+        j++;    
+        }
+        while(j<0);
+        for(k=0;k<0;k++){
+            System.out.println(k);
+        }
+        
+        System.out.println("false condition");
+    }
+    
+}
