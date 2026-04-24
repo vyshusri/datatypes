3437d0712cc3e5bcb9232a7dacecb0779db974f2
vyshusri <vyshusriram15@gmail.com>
2026-04-24 01:33:11 -0700

    whileloop

---
+++ logicCheck.java
@@ -0,0 +1,10 @@
+public class logicCheck {
+    static String username="sriram";
+    static int password=1234;
+    public static void main(String[] args) {
+        if(username.equals("sriram")(password.euals(1234))||username.equals("sairam")){
+
+        }
+    }
+    
+}
---
+++ whileLoop.java
@@ -0,0 +1,18 @@
+public class whileLoop {
+    public static void main(String[] args){
+        int i=1;
+        while(i<=10){
+            System.out.println(i);
+            i++;
+
+        }
+         int j=10;
+        while(j>=1){
+            System.out.println(j);
+            j--;
+
+        }
+       
+    }
+    
+}
