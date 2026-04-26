f3d471157652811d9ce7105314979ab76c09a8ef
vyshusri <vyshusriram15@gmail.com>
2026-04-26 10:30:45 -0700

    logicheck

--- logicCheck.java
+++ logicCheck.java
@@ -1,8 +1,8 @@
 public class logicCheck { 
     public static void main(String[] args) {
-        string username="sairam";
-        int password=1234;
-System.out.println((username.equals("sairam")&& password.equals("1234")?"user name is correct":"password is corrct"));
+        String username="sairam";
+        String password="sri";
+System.out.println(username.equals("sairam")&& password.equals("sri")?"user name is correct":"password is corrct");
         }
     
     }
