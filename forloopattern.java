c0a9a330b5c565c87075aa3d3adf030b3166b632
vyshusri <vyshusriram15@gmail.com>
2026-04-26 10:24:22 -0700

    trianglepattern using forloop

--- forloopattern.java
+++ forloopattern.java
@@ -1,13 +1,13 @@
 public class forloopattern{
     public static void main(String[] args){
         for(int i=1; i<=6; i++){
-            for(int j=1; j<=6; j++){
+            for(int j=1; j<=i; j++)
+                for(int k=1;k<=j;k++){
                 System.out.print("*");
             }
             System.out.println();
         }
     }
-   
 }
  
  
--- logicCheck.java
+++ logicCheck.java
@@ -2,8 +2,9 @@ public class logicCheck {
     public static void main(String[] args) {
         string username="sairam";
         int password=1234;
-System.out.println((username.equals("sairam")&&password.equals("1234")?"user name is correct":"password is corrct"));
+System.out.println((username.equals("sairam")&& password.equals("1234")?"user name is correct":"password is corrct"));
         }
+    
     }
     
 
