5057dd68eed29730f55ddeecf931ce69d4120e13
vyshusri <vyshusriram15@gmail.com>
2026-04-30 09:26:55 -0700

    object (car)

---
+++ Car.java
@@ -0,0 +1,26 @@
+public class Car {
+    String brand;
+    String model;
+    int year;
+    public static void main(String[] args){
+        Car car1=new Car();
+        car1.brand="vaishnavi";
+        car1.model="sriram";
+        car1.year=2027;
+
+        Car car2=new Car();
+        car2.brand="Sahasthra";
+        car2.model="guddu";
+        car2.year=2028;
+        
+        System.out.println(car1.brand);
+        System.out.println(car1.model);
+        System.out.println(car1.year);
+        System.out.println(car2.brand);
+        System.out.println(car2.model);
+        System.out.println(car2.year);
+    }
+
+    }
+    
+
