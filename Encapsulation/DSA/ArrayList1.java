00c0892f53eb257f8defc4bede5e2534d66c6b17
vyshusri <vyshusriram15@gmail.com>
2026-05-23 06:28:31 -0700

    ArrayList

---
+++ Encapsulation/DSA/ArrayList1.java
@@ -0,0 +1,27 @@
+package Encapsulation.DSA;
+import java.util.ArrayList; 
+
+    public class ArrayList1 {
+    public static void main(String[] args) {
+        
+    
+    ArrayList<String> fruits = new ArrayList<>(); 
+    
+    fruits.add("Apple");
+    fruits.add("Pineapple");
+    fruits.add("Orange");
+    fruits.add(0,"Banana");
+    fruits.set(1,"watermelon");
+    fruits.remove(0);
+    
+    System.out.println("Fruits:"+fruits);
+    System.out.println("Friuts:"+fruits.get(1));
+    System.out.println("Friuts:"+fruits.isEmpty());
+    System.out.println(fruits.size());
+
+
+    
+    }
+}
+    
+
